4eec36d579ad47b1f1a0d735ca956c2c96024eca
Michael Bestas <mkbestas@gmail.com>
2023-04-04 22:10:12 +0300

    Initial CalyxOS makefile
    Change-Id: I38c16b6e2a1da7a16ad31b8edae5fec7701b480a

---
+++ calyx_rhode.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common stuff
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit from device
+$(call inherit-product, device/motorola/rhode/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := calyx_rhode
+PRODUCT_DEVICE := rhode
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g52
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+BUILD_FINGERPRINT := "motorola/rhode_g/rhode:12/S1SRS32.38-132-14/c4430-03865f:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=rhode_g \
+    PRIVATE_BUILD_DESC="rhode_g-user 12 S1SRS32.38-132-14 c4430-03865f release-keys"
